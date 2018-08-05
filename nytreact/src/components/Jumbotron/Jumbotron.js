8765a6c53f860c6c0c8278b9f0553b3546baba33
Mohammed  Fouad <you@example.com>
2018-08-05 18:30:19 -0400

    up

---
+++ nytreact/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+  <div className="jumbotron text-center">
+    <h1>React Recipes</h1>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://nytarticle-search-fsf.herokuapp.com/"
+    >
+     Search for an Article
+    </a>
+  </div>
+);
+
+export default Jumbotron;
---
+++ nytreact/src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => (
+  <nav className="navbar navbar-dark bg-dark">
+    <a className="navbar-brand" href="/">
+      New York Times
+    </a>
+  </nav>
+);
+
+export default Nav;
